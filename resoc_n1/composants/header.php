7a20c83d81638755e078b72727ecc0506610fb16
mimi <jeremyzynger@gmail.com>
2023-02-20 11:03:49 +0100

    add logout btn

--- resoc_n1/composants/header.php
+++ resoc_n1/composants/header.php
@@ -1,3 +1,9 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['logout'])) {
+    session_destroy();
+    header('Refresh:0');
+}
+?>
 <html lang="fr">
 
 <head>
@@ -15,6 +21,7 @@
             <a href="wall.php">Mur</a>
             <a href="feed.php">Flux</a>
             <a href="tags.php">Mots-clés</a>
+            <a href="usurpedpost.php">Messages</a>
         </nav>
         <nav id="user">
             <a href="#">Profil</a>
@@ -23,6 +30,11 @@
                 <li><a href="settings.php?user_id=5">Paramètres</a></li>
                 <li><a href="followers.php?user_id=5">Mes suiveurs</a></li>
                 <li><a href="subscriptions.php?user_id=5">Mes abonnements</a></li>
+                <?php if (isset($_SESSION['connected_id'])) { ?>
+                    <form method="post">
+                        <button type="sumbit" name="logout">Logout</button>
+                    </form>
+                <?php } ?>
             </ul>
 
         </nav>
